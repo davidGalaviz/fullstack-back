b23eeb93c3af4aca765a018a906bf658a9cb139b
davidGalaviz <110849310+davidGalaviz@users.noreply.github.com>
2023-01-05 16:30:46 -0600

    corregir errores de pylint en persistencia

--- persistencia.py
+++ persistencia.py
@@ -1,4 +1,12 @@
+"""
+Módulo de Persistencia
+"""
+
 def guardar_pedido(nombre, apellidos):
+    """
+    Guarda el pedido en el archivo pedidos.txt
+    """
+    
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
-        file.close
+        file.close()

711847348c68225e8e6eb75637297562dfef3cf4
davidGalaviz <110849310+davidGalaviz@users.noreply.github.com>
2023-01-05 16:41:51 -0600

    guardar el nombre con un guión al inicio

--- persistencia.py
+++ persistencia.py
@@ -7,5 +7,5 @@ def guardar_pedido(nombre, apellidos):
     Guarda el pedido en el archivo pedidos.txt
     """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellidos + "\n")
+        file.write("-" + nombre + " " + apellidos + "\n")
         file.close()

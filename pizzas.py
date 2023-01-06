0c8df190d9a472dac601f4e3707f1a42ffdd2dbd
davidGalaviz <110849310+davidGalaviz@users.noreply.github.com>
2023-01-06 10:28:24 -0600

    añadir el endpoint checksize

--- pizzas.py
+++ pizzas.py
@@ -2,7 +2,7 @@
 API de Pizzas
 """
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 
 import persistencia
 
@@ -22,3 +22,14 @@ def solicitar_pizza():
     persistencia.guardar_pedido(nombre, apellidos)
 
     return redirect("http://localhost/solicita_pedido.html", code=302)
+
+@app.route("/checksize", methods=["POST"])
+def checksize():
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    """
+    size = request.form.get("size")
+
+    mensaje = "No disponible" if size == "S" else "Disponible"
+
+    return Response(mensaje, 200, {"Access-Control-Allow-Origin": "*"})
